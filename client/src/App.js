0b719a7822689dd8bb274b67b4df5b87312ae8c6
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-22 20:30:29 +0100

    side bar slides on with icon click using collapsible

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { Box, Button, Heading, Grommet } from 'grommet';
+import { useState } from "react";
+import { Box, Button, Heading, Grommet, Collapsible } from 'grommet';
 import { Chrome } from 'grommet-icons';
 import { Normalize } from 'styled-normalize';
 
@@ -19,19 +20,42 @@ const theme = {
 };
 
 export default function App() {
+    const [showSidebar, setShowSidebar] = useState(false);
+
     return (
         <>
             <Typography />
-            <Grommet theme={theme}>
+            <Grommet theme={theme} full>
                 <Normalize />
-                <AppBar>
-                    Alistair Quinn
-                    <Heading level='3' margin='none'>Oratorium</Heading>
-                    <Button
-                        icon={<Chrome />}
-                        onClick={() => { }}
-                    />
-                </AppBar>
+                <Box fill>
+
+                    <AppBar>
+                        Alistair Quinn
+                        <Heading level='3' margin='none'>Oratorium</Heading>
+                        <Button
+                            icon={<Chrome />}
+                            onClick={() => setShowSidebar(!showSidebar)}
+                        />
+                    </AppBar>
+
+                    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+                        <Box flex align='center' justify='center'>
+                            main body
+                        </Box>
+                        <Collapsible direction="horizontal" open={showSidebar}>
+                            <Box
+                                flex
+                                width='medium'
+                                background='light-2'
+                                elevation='small'
+                                align='center'
+                                justify='center'
+                            >
+                                side bar
+                            </Box>
+                        </Collapsible>
+                    </Box>
+                </Box>
             </Grommet >
         </>
     );
