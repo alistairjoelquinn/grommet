fada2c5255d65e6f5dc0141860024536bf640a0d
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-22 21:52:39 +0100

    side bar response to browser size

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Button, Heading, Grommet, Collapsible } from 'grommet';
+import { Box, Button, Heading, Grommet, Collapsible, ResponsiveContext, Layer } from 'grommet';
 import { Chrome } from 'grommet-icons';
 import { Normalize } from 'styled-normalize';
 
@@ -24,38 +24,55 @@ export default function App() {
 
     return (
         <>
+            <Normalize />
             <Typography />
             <Grommet theme={theme} full>
-                <Normalize />
-                <Box fill>
+                <ResponsiveContext.Consumer>
+                    {size => (
+                        <Box fill>
 
-                    <AppBar>
-                        Alistair Quinn
-                        <Heading level='3' margin='none'>Oratorium</Heading>
-                        <Button
-                            icon={<Chrome />}
-                            onClick={() => setShowSidebar(!showSidebar)}
-                        />
-                    </AppBar>
+                            <AppBar>
+                                Alistair Quinn
+                                <Heading level='3' margin='none'>Oratorium</Heading>
+                                <Button
+                                    icon={<Chrome />}
+                                    onClick={() => setShowSidebar(!showSidebar)}
+                                />
+                            </AppBar>
 
-                    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-                        <Box flex align='center' justify='center'>
-                            main body
-                        </Box>
-                        <Collapsible direction="horizontal" open={showSidebar}>
-                            <Box
-                                flex
-                                width='medium'
-                                background='light-2'
-                                elevation='small'
-                                align='center'
-                                justify='center'
-                            >
-                                side bar
+                            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+                                <Box flex align='center' justify='center'>
+                                    main body
+                                </Box>
+                                {(!showSidebar || size !== 'small') ? (
+                                    <Collapsible direction="horizontal" open={showSidebar}>
+                                        <Box
+                                            flex
+                                            width='medium'
+                                            background='light-2'
+                                            elevation='small'
+                                            align='center'
+                                            justify='center'
+                                        >
+                                            side bar
+                                        </Box>
+                                    </Collapsible>
+                                ) : (
+                                    <Layer >
+                                        <Box
+                                            fill
+                                            background='light-2'
+                                            align='center'
+                                            justify='center'
+                                        >
+                                            sidebar
+                                        </Box>
+                                    </Layer>
+                                )}
                             </Box>
-                        </Collapsible>
-                    </Box>
-                </Box>
+                        </Box>
+                    )}
+                </ResponsiveContext.Consumer>
             </Grommet >
         </>
     );
