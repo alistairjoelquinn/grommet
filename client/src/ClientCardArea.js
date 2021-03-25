8734182cffe89b62855a2a2053cfb8b698464fe5
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-25 23:28:23 +0100

    grid switched out for regular grid

--- client/src/ClientCardArea.js
+++ client/src/ClientCardArea.js
@@ -1,37 +1,45 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Grid, ResponsiveContext } from "grommet";
+import { Button, Card, CardBody, CardFooter, CardHeader } from "grommet";
 import { Favorite, ShareOption } from "grommet-icons";
+import styled from "styled-components";
 
 import cardData from '../data/cardData';
 
+const GridStyles = styled.div`
+    display: grid;
+    width: 100%;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 200px);
+    grid-auto-rows: 200px;
+    gap: 2rem;
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+`;
+
 const ClientCardArea = () => {
     return (
-        <ResponsiveContext.Consumer>
-            {size => (
-                <Grid
-                    rows={size !== "small" ? ["small", "small"] : ["small", "small", "small"]}
-                    columns={size !== "small" ? ["small", "small", "small"] : ["small", "small"]}
-                    gap="3vw"
-                    width="100%"
-                >
-                    {cardData.map(card => (
-                        <Card key={card.name} height="small" width="small" background="light-1" gap="none">
-                            <CardHeader pad="20px">{card.name}</CardHeader>
-                            <CardBody pad="20px" style={{ fontSize: '0.8rem', paddingTop: '0' }}>{card.job}</CardBody>
-                            <CardFooter pad={{ horizontal: "small" }} background="light-2">
-                                <Button
-                                    icon={<Favorite color="red" />}
-                                    hoverIndicator
-                                />
-                                <Button
-                                    icon={<ShareOption color="plain" />}
-                                    hoverIndicator
-                                />
-                            </CardFooter>
-                        </Card>
-                    ))}
-                </Grid>
-            )}
-        </ResponsiveContext.Consumer>
+        <GridStyles>
+            {cardData.map(card => (
+                <div key={card.name}>
+                    <Card height="small" width="small" background="light-1" gap="none">
+                        <CardHeader pad="20px">{card.name}</CardHeader>
+                        <CardBody pad="20px" style={{ fontSize: '0.8rem', paddingTop: '0' }}>{card.job}</CardBody>
+                        <CardFooter pad={{ horizontal: "small" }} background="light-2">
+                            <Button
+                                icon={<Favorite color="red" />}
+                                hoverIndicator
+                            />
+                            <Button
+                                icon={<ShareOption color="plain" />}
+                                hoverIndicator
+                            />
+                        </CardFooter>
+                    </Card>
+                </div>
+            ))}
+        </GridStyles>
     );
 };
 
