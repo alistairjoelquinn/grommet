ffa1e925f317aef8941334d34aaf206bb903ef6b
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-25 19:22:05 +0100

    dummy data added to testing cards

---
+++ client/data/cardData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        name: 'Alistair Quinn',
+        job: 'Professional Web Development Services'
+    },
+    {
+        name: 'Sarah Walker',
+        job: 'Award-winning Architect'
+    },
+    {
+        name: 'Brian Smith',
+        job: 'Footballer Turned English Teacher'
+    },
+    {
+        name: 'Amy Jones',
+        job: 'Graphic Designer, UX/UI Expert'
+    },
+    {
+        name: 'Lars Montague',
+        job: 'Criminal Defence Lawyer'
+    },
+    {
+        name: 'Craig Wallerman',
+        job: 'Office Supplies Distribution Consultant'
+    },
+];
--- client/src/MainHomeArea.js
+++ client/src/MainHomeArea.js
@@ -1,5 +1,7 @@
-import { Box, Button, Card, CardBody, CardFooter, CardHeader } from "grommet";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Grid } from "grommet";
 import { Favorite, ShareOption } from "grommet-icons";
+
+import cardData from '../data/cardData';
 
 const MainHomeArea = () => {
     return (
@@ -8,20 +10,33 @@ const MainHomeArea = () => {
                 <Button primary label="Click Me" />
                 <Button secondary label="Click Me Next" />
             </Box>
-            <Card height="small" width="small" background="light-1" gap="none">
-                <CardHeader pad="20px">Alistair Quinn</CardHeader>
-                <CardBody pad="20px" style={{ fontSize: '0.8rem', paddingTop: '0' }}>Professional Web Development Services</CardBody>
-                <CardFooter pad={{ horizontal: "small" }} background="light-2">
-                    <Button
-                        icon={<Favorite color="red" />}
-                        hoverIndicator
-                    />
-                    <Button
-                        icon={<ShareOption color="plain" />}
-                        hoverIndicator
-                    />
-                </CardFooter>
-            </Card>
+            <Grid
+                rows={['xxsmall', 'xsmall']}
+                columns={['xsmall', 'small']}
+                gap="small"
+                areas={[
+                    { name: 'header', start: [0, 0], end: [1, 0] },
+                    { name: 'nav', start: [0, 1], end: [0, 1] },
+                    { name: 'main', start: [1, 1], end: [1, 1] },
+                ]}
+            >
+                {cardData.map(card => (
+                    <Card key={card.name} height="small" width="small" background="light-1" gap="none">
+                        <CardHeader pad="20px">{card.name}</CardHeader>
+                        <CardBody pad="20px" style={{ fontSize: '0.8rem', paddingTop: '0' }}>{card.job}</CardBody>
+                        <CardFooter pad={{ horizontal: "small" }} background="light-2">
+                            <Button
+                                icon={<Favorite color="red" />}
+                                hoverIndicator
+                            />
+                            <Button
+                                icon={<ShareOption color="plain" />}
+                                hoverIndicator
+                            />
+                        </CardFooter>
+                    </Card>
+                ))}
+            </Grid>
         </Box >
     );
 };
