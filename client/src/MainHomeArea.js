96ee62ada9f33901f741b84e2c0a61fc689572e5
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-25 19:09:44 +0100

    main area card appearing in correct place now

--- client/src/MainHomeArea.js
+++ client/src/MainHomeArea.js
@@ -3,23 +3,26 @@ import { Favorite, ShareOption } from "grommet-icons";
 
 const MainHomeArea = () => {
     return (
-        <Box flex align='center' justify='center' gap="10px" direction="column">
-            <Box flex align='center' justify='center' gap="10px" direction="row">
+        <Box flex align='center' justify='center' gap="4rem" direction="column" height>
+            <Box align='center' justify='center' gap="10px" direction="row">
                 <Button primary label="Click Me" />
                 <Button secondary label="Click Me Next" />
             </Box>
-            <Card height="small" width="small" background="light-1">
-                <CardHeader pad="medium">Header</CardHeader>
-                <CardBody pad="medium">Body</CardBody>
+            <Card height="small" width="small" background="light-1" gap="none">
+                <CardHeader pad="20px">Alistair Quinn</CardHeader>
+                <CardBody pad="20px" style={{ fontSize: '0.8rem', paddingTop: '0' }}>Professional Web Development Services</CardBody>
                 <CardFooter pad={{ horizontal: "small" }} background="light-2">
                     <Button
                         icon={<Favorite color="red" />}
                         hoverIndicator
                     />
-                    <Button icon={<ShareOption color="plain" />} hoverIndicator />
+                    <Button
+                        icon={<ShareOption color="plain" />}
+                        hoverIndicator
+                    />
                 </CardFooter>
             </Card>
-        </Box>
+        </Box >
     );
 };
 
